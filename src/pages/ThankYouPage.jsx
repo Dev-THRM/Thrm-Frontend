00fcc447cf160c97c30ed905cd0c27038b7244d7
Sneha Dewani <snehadewani07@gmail.com>
2026-08-07 17:21:41 +0530

    removed duplicate useEffect

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { TicketCheck } from "lucide-react";
-import { useEffect } from "react";
 
 export const TYP = () => {
     useEffect(() => {
